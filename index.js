db4f289f75f68b009e6f802345c66fe92b230fb4
Alex Jin <jin.alex123@gmail.com>
2017-07-31 17:18:53 -0400

    Build messages using babel inside this module (#1)
    * Add gitignore
    
    * Attempt to do rimraf and babel in build command
    
    * Missed a callback for rimraf
    
    * Use babel to build messages inside the module
    
    * Update README

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
+'use strict'
+
+var path = require('path');
 var program = require('commander');
+var config = require(path.join(path.resolve('.'), 'lang.conf.js'));
+
+function build () {
+  var spawn = require( 'child_process' ).spawnSync;
+  var babel = spawn('babel', ['./src']);
+  var buildTranslations = require('./lib/translator');
+  buildTranslations.default();
+  console.log('Translations built');
+}
 
 program
-  .version('1.0.0')
+  .version('1.1.0')
 
 program
   .command('build')
   .description('Build translations for react-intl')
-  .action(function() {
-    var buildTranslations = require('./lib/translator');
-    console.log('Translations built');
-    buildTranslations.default();
-  });
+  .action(function () {build()});
 
 program
   .command('export')
   .description('Export current messages to Google Sheets')
   .action(function() {
     console.log('Exporting messages and translations...');
-    var buildTranslations = require('./lib/translator');
-    buildTranslations.default();
+    build()
     var exportMessages = require('./lib/exportMessages');
     exportMessages.default();
   });
